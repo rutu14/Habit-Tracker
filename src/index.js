f1702e8e48cdfad93c1b661d43866337915aa438
rutu14 <vidyapunjabi7@gmail.com>
2022-07-08 20:57:06 +0530

    Initial Setup

--- src/index.js
+++ src/index.js
@@ -3,12 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter } from "react-router-dom";
 
-// Call make Server
 makeServer();
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  	<React.StrictMode>
+		<BrowserRouter>
+			<ChakraProvider> 
+				<Provider store={store}>   
+					<App />
+				</Provider>
+			</ChakraProvider>
+		</BrowserRouter>
+  	</React.StrictMode>,
+  	document.getElementById("root")
 );
